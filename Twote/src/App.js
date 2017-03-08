84f84cf80b1e66cd90058df37859104b9cdabb42
Zachary Kent <zak.kent@gmail.com>
2017-03-08 11:11:43 -0800

    added example form

--- Twote/src/App.js
+++ Twote/src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import TweetList from './components/tweetList';
+import TweetForm from './components/tweetUpdateForm';
 import './App.css';
 
 
 
 class App extends Component {
+  handleSubmit = (values) => {
+    // Do something with the form values
+    console.log(values);
+  }
   render() {
     return (
       
       <div>
           <TweetList />
+          <TweetForm onSubmit={this.handleSubmit} />
       </div>
 
     );
