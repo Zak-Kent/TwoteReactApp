7a3b999aced3f0c94a36a3524e6eaab9417176c8
Zachary Kent <zak.kent@gmail.com>
2017-03-15 20:39:26 -0700

    navbar changing page

--- Twote/src/App.js
+++ Twote/src/App.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
 import TweetList from './components/tweetList';
-// import MyNavbar from './components/myNavbar';
+import MyNavbar from './components/myNavbar';
+import { apiGetTweets } from './actions/apiTweets';
 import './App.css';
 
 
 
 class App extends Component {
 
+  constructor() {
+    super();
+    this.displayTweets = this.displayTweets.bind(this);
+  }
+
+  // make method that sets up initial api call 
+  displayTweets(approveValue) {
+    // should be 1 or 0
+
+    console.log("inside app method", approveValue)
+    let val = approveValue;
+    let url = `http://localhost:8000/twitter/tweets/?approved=${val}`;
+    console.log(url);
+
+    this.props.dispatch(apiGetTweets(url))
+
+  }
+
   render() {
     return (
       
       <div>
+          <MyNavbar displayTweets={this.displayTweets}/>
           <TweetList />          
       </div>
 
@@ -18,5 +40,11 @@ class App extends Component {
   }
 }
 
+function mapStateToProps(state) {
+  return { 
+    tweets: state.tweets, 
+  };
+}
 
-export default App;
+export default connect(mapStateToProps)(App);
+// export default App;
--- Twote/src/components/myNavbar.js
+++ Twote/src/components/myNavbar.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import { Nav, NavItem } from 'react-bootstrap';
 
-const MyNavbar = React.createClass({
+
+export class MyNavbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+
   handleSelect(eventKey) {
     event.preventDefault();
-    alert(`selected ${eventKey}`);
-  },
+    this.props.displayTweets(eventKey)
+
+  }
 
   render() {
     return (
       <Nav bsStyle="tabs" activeKey="1" onSelect={this.handleSelect}>
         <NavItem eventKey="1" href="/home">NavItem 1 content</NavItem>
         <NavItem eventKey="2" title="Item">NavItem 2 content</NavItem>
-        <NavItem eventKey="3" disabled>NavItem 3 content</NavItem>
       </Nav>
     );
   }
-});
+};
+
+export default MyNavbar;
